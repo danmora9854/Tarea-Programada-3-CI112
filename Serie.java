cde900bedde19907bce6b1896930224d7a935407
danmora9854 <dan.mora9854@gmail.com>
2021-07-05 13:48:44 -0600

    Create Serie.java

---
+++ Serie.java
@@ -0,0 +1,23 @@
+import java.util.ArrayList;
+/**
+ * 
+ *
+ * @author (Juan Jose Bermúdez carné Daniel Mora Mora B64720 Diego Hernández carné)
+ * @version (5-07-2021)
+ */
+public class Serie
+{
+    int show_id;
+    String tipo;
+    String titulo;
+    String director;
+    ArrayList<String> cast;
+    String pais;
+    String fecha;
+    String año;
+    String audiencia;
+    String duracion;
+    String categoria;
+    String contenido;
+    
+}
