2338da86404f7e487e865489718252f48fbe0124
JJBV-UCR <81998140+JJBV-UCR@users.noreply.github.com>
2021-07-05 21:44:34 -0600

    Update Cast.java

--- Cast.java
+++ Cast.java
@@ -7,4 +7,12 @@ public class Cast
     String actor;
     ArrayList <ID> ids;
     Cast actor_izq, actor_der;//ordenadas segun el metodo de comparacion de strings compareTo() para hacer un arbol binario de busqueda
+    
+    public Cast (String actor)
+    {
+        this.actor = actor;
+        ids = new ArrayList <ID> ();
+        actor_izq = null;
+        actor_der = null;
+    }
 }
