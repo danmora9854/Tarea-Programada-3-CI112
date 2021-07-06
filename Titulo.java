9d58e94e557940a5143a706c876eedd49886f9f1
JJBV-UCR <81998140+JJBV-UCR@users.noreply.github.com>
2021-07-05 21:50:47 -0600

    Update Titulo.java

--- Titulo.java
+++ Titulo.java
@@ -7,4 +7,12 @@ public class Titulo
     String title_name;
     ID show_id;
     Titulo title_izq, title_der;//ordenadas segun el metodo de comparacion de strings compareTo() para hacer un arbol binario de busqueda
+    
+    public Titulo (String title_name)
+    {
+        this.title_name = title_name;
+        show_id = null;
+        title_izq = null;
+        title_der = null;
+    }
 }
