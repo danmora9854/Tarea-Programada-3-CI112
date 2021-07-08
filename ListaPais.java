00af2161597ff012e13902e3a415418600495b99
danmora9854 <dan.mora9854@gmail.com>
2021-07-07 22:26:52 -0600

    Update ListaPais.java

--- ListaPais.java
+++ ListaPais.java
@@ -1,1 +1,25 @@
-
+public class ListaPais{
+Pais first;
+Pais last;
+public ListaPais(){
+  first=last=null;
+}
+  public void append(String  name){ //agrega al final
+        if (first==null){
+            first=last=new Pais(name);
+        }else{
+            Pais tmp=last;
+            if (first==last){
+                last=new Pais(name);
+                first.next=last;
+                last.prev=tmp;
+            }else{
+                last.next=new Pais(name);
+                last=last.next;
+                last.prev=tmp;
+            }
+        }
+    }
+ 
+  
+}
