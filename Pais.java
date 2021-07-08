a7a66b3cc826942201a2e2367bc7778b988e8923
danmora9854 <dan.mora9854@gmail.com>
2021-07-07 22:25:07 -0600

    Update Pais.java

--- Pais.java
+++ Pais.java
@@ -7,11 +7,12 @@ public class Pais
     String pais_name;
     ArrayList <ID> ids; 
     Pais next;
-    
+    Pais prev;
     public Pais (String pais_name)
     {
         this.pais_name = pais_name;
         ids = new ArrayList <ID> ();
         next = null;
+        prev = null;
     }
 }
