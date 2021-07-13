56fd4d4feb84f5dff3f2314ff9d2873b3ec55fe4
JJBV-UCR <81998140+JJBV-UCR@users.noreply.github.com>
2021-07-13 13:42:34 -0600

    Update ID.java

--- ID.java
+++ ID.java
@@ -5,14 +5,15 @@ import java.util.ArrayList;
  */
 public class ID
 {
-    int value;
-    Categoria categ;
+    String value;
+    Titulo title;
+    ArrayList <Categoria> categ;
     Pais country;
     ArrayList <Cast> cast;
     String tipo, director, fecha, año, audiencia, duracion, descripcion;
     ID next;
     
-    public ID (int value, String tipo, String director, String fecha, String año, String audiencia, String duracion, String descripcion)
+    public ID (String value, String tipo, String director, String fecha, String año, String audiencia, String duracion, String descripcion)
     {
         this.value = value;
         this.tipo = tipo;
@@ -22,5 +23,9 @@ public class ID
         this.audiencia = audiencia;
         this.duracion = duracion;
         this.descripcion = descripcion;
+        categ = new ArrayList <Categoria> ();
+        cast = new ArrayList <Cast> ();
+        title = null;
+        country = null;
     }
 }
