37df85e1bef19842beaa43217c08cbcaf92b0904
JJBV-UCR <81998140+JJBV-UCR@users.noreply.github.com>
2021-07-20 14:25:44 -0600

    Update Cast.java

--- Cast.java
+++ Cast.java
@@ -1,59 +1,62 @@
-import java.util.ArrayList;
 /**
  * Arbol binario de busqueda para buscar por miembro del cast
  */
+import java.util.ArrayList;
 public class Cast
 {
     String actor;
     ArrayList <ID> ids;
     Cast actor_izq, actor_der;//ordenadas segun el metodo de comparacion de strings compareTo() para hacer un arbol binario de busqueda
     
-    public Cast (String actor)
+    public Cast (String actor, ID id)
     {
         this.actor = actor;
         ids = new ArrayList <ID> ();
+        ids.add(id);
         actor_izq = null;
         actor_der = null;
     }
     
-    public String muestreActor(){
-        
-        return actor + "Participó en las siguientes películas:\n" + ids;
-    
-    }
-    
-    public Cast buscarActor(String act)
-    {
-        Cast c = null;  
-        if (act.compareTo(actor) == 0) 
-            c=this;
-        else{
-            if (act.compareTo(actor) < 0){ 
-                if (actor_der!=null)
-                    return actor_der. buscarActor(act);
-            }else{
-                if (actor_izq!=null)
-                    return actor_izq. buscarActor(act);
-            }
-        }
-        return c;
-    }
-    
-    public void insertarActor(String act)
-    {
-        if (act.compareTo(actor) < 0){                            
+    public void add (String nombre, ID id_num) {
+        if (nombre.compareTo(actor) < 0){                            
             if (actor_der == null){
-                actor_der = new Cast(act);
+                actor_der = new Cast(nombre, id_num);
+                id_num.cast.add(actor_der);
             }else{
-                actor_der.insertarActor(act);
+                actor_der.add(nombre, id_num);
             }
         }else{
-            if ((act.compareTo(actor) > 0) && (actor == null)){ 
-                actor_izq = new Cast(act);
+            if ((nombre.compareTo(actor) > 0) && (actor_izq == null)){ 
+                actor_izq = new Cast(nombre, id_num);
+                id_num.cast.add(actor_izq);
             }else{
-                if (act.compareTo(actor) != 0)                     
-                    actor_izq.insertarActor(act);
+                if (nombre.compareTo(actor) != 0){                   
+                    actor_izq.add(nombre, id_num);
+                }else{
+                    ids.add(id_num);
+                    id_num.cast.add(this);
+                }
             }
         }
+    }
+    
+    /**
+     * Método que busca si existe cierto actor en el arbol.
+     * De ser así devuelve el objeto Cast. De lo contrario devuelve null.
+     */
+    public Cast busque(String nombre){
+        Cast r = null;  
+        if (nombre.compareTo(actor) == 0) 
+            r=this;
+        else{
+            if (nombre.compareTo(actor) < 0){ 
+                if (actor_der!=null)
+                    return actor_der.busque(nombre);
+            }else{
+                if (actor_izq!=null)
+                    return actor_izq.busque(nombre);
+            }
+        }
+        return r;
     }
 }
