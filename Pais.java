27c1eb33bbdfcb1604e47029dce4eb5c9099e8b1
danmora9854 <dan.mora9854@gmail.com>
2021-07-20 22:47:27 -0600

    Update Pais.java

--- Pais.java
+++ Pais.java
@@ -4,10 +4,14 @@
 import java.util.ArrayList;
 public class Pais
 {
-    String pais_name;
-    ArrayList <ID> ids; 
-    Pais next;
-    
+    String pais_name; //Nombre del pais
+    ArrayList <ID> ids; //Arreglo de objetos tipo ID
+    Pais next; //Siguiente nodo tipo Pais de la lista
+     /**
+     * Constructor del objeto tipo Pais
+     * @param pais_name: Recibe un string que sera el nombre del pais 
+     * @param id: Recibe un objeto tipo ID para asignarle al pais en el arreglo ids.
+     */
     public Pais (String pais_name, ID id)
     {
         this.pais_name = pais_name;
@@ -15,7 +19,12 @@ public class Pais
         ids.add(id);
         next = null;
     }
-    
+     /**
+     * add: Método que añade un objeto tipo pais a la lista simple
+     * 
+     * @param pais: Recibe un string que es el nombre del nuevo pais 
+     * @param id_num: Recibe un objeto tipo ID para asignarle al pais.
+     */
     public void add (String pais, ID id_num) {
         if (pais.equals(pais_name)) {
             ids.add(id_num);
@@ -28,7 +37,11 @@ public class Pais
             next.add(pais,id_num); //cualquier otro caso
         }
     }
-
+     /**
+     * existe: Método que devuelve un boolean que indica si un pais existe en la lista. 
+     * 
+     * @param n: Recibe un string que es el nombre del pais por buscar.
+     */
     public boolean existe(String n){
         boolean flag=false;
         if(pais_name==n){
@@ -38,6 +51,11 @@ public class Pais
         }
         return flag;
     }
+     /**
+     * buscarPais: Método que busca un pais por nombre y una vez que lo encuentra, devuelve su lista de IDs asociados. 
+     * Si el pais que se esta buscando no existe, se imprime un mensaje de aviso.
+     * @param n: Recibe un string que es el nombre del pais por buscar.
+     */
     public ArrayList<ID> buscarPais(String n){
         if(existe(n)){
             if(pais_name==n){
