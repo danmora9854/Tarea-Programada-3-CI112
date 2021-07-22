ad59e10aadc89a2c0eafa133d60a43077cfea4a0
JJBV-UCR <81998140+JJBV-UCR@users.noreply.github.com>
2021-07-21 22:00:43 -0600

    Update Controlador.java

--- Controlador.java
+++ Controlador.java
@@ -254,7 +254,7 @@ public class Controlador
     public static void main (String arg[])
     {
         Controlador c = new Controlador ();
-        String[] fileChoice = {"Cargar la data total","Cargar las primeras 25 entradas"};
+        String[] fileChoice = {"Cargar la data total","Cargar las primeras 100 entradas"};
         String ansFile = (String)(JOptionPane.showInputDialog(null,"Cómo desea cargar la data?","Por favor escoja una opción",JOptionPane.QUESTION_MESSAGE, null,fileChoice,fileChoice[0]));
         String file_name = (ansFile.equals("Cargar la data total"))?"netflix_titles.txt":"netflix_titles(cortada)";
         c.leaData(file_name);
